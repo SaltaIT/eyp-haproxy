31bf251845ab7f112778e12804bca9ace60a6b14
Jordi Prats <jordi.prats@gmail.com>
2017-03-17 18:57:26 +0100

    skel haproxy

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,3 +1,83 @@
 class haproxy::config inherits haproxy {
 
+  file { '/etc/haproxy/haproxy.cfg':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template("${module_name}/haproxycfg.erb"),
+  }
+
+  file { '/etc/haproxy/errors':
+    ensure => 'directory',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+
+  #400.http  403.http  408.http  500.http  502.http  503.http  504.http
+
+  file { '/etc/haproxy/errors/400.http':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => File['/etc/haproxy/errors'],
+    source  => "puppet:///modules/${module_name}/errors/400.http"
+  }
+
+  file { '/etc/haproxy/errors/403.http':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => File['/etc/haproxy/errors'],
+    source  => "puppet:///modules/${module_name}/errors/403.http"
+  }
+
+  file { '/etc/haproxy/errors/408.http':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => File['/etc/haproxy/errors'],
+    source  => "puppet:///modules/${module_name}/errors/408.http"
+  }
+
+  file { '/etc/haproxy/errors/500.http':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => File['/etc/haproxy/errors'],
+    source  => "puppet:///modules/${module_name}/errors/500.http"
+  }
+
+  file { '/etc/haproxy/errors/502.http':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => File['/etc/haproxy/errors'],
+    source  => "puppet:///modules/${module_name}/errors/502.http"
+  }
+
+  file { '/etc/haproxy/errors/503.http':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => File['/etc/haproxy/errors'],
+    source  => "puppet:///modules/${module_name}/errors/503.http"
+  }
+
+  file { '/etc/haproxy/errors/504.http':
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => File['/etc/haproxy/errors'],
+    source  => "puppet:///modules/${module_name}/errors/504.http"
+  }
+
 }
---
+++ manifests/listen.pp
@@ -0,0 +1,7 @@
+# listen push 0.0.0.0:22
+# 	mode tcp
+# 	balance roundrobin
+# 	server honeypot1 172.16.101.6:2222
+define haproxy::listen() {
+  
+}
