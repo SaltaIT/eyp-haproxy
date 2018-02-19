5a50e70325f58ff57b0ada75b8c9ae06f318382d
Jordi Prats <jordi.prats@gmail.com>
2018-02-19 17:31:44 +0100

    nt

--- manifests/balancer.pp
+++ manifests/balancer.pp
@@ -4,10 +4,6 @@ define haproxy::balancer(
                           $mode          = 'tcp',
                           $balance_mode  = 'roundrobin',
                         ) {
-  # listen push 0.0.0.0:22
-  # 	mode tcp
-  # 	balance roundrobin
-  # 	server honeypot1 172.16.101.6:2222
   concat::fragment{ "balancer: ${balancer_name}":
     target  => '/etc/haproxy/haproxy.cfg',
     order   => "99-${balancer_name}-0",
