8815808013d685844412d65bcc4fe4c41ad53ac5
Jordi Prats <jordi.prats@gmail.com>
2019-02-22 15:33:46 +0100

    ordre de servers

--- manifests/balancer/server.pp
+++ manifests/balancer/server.pp
@@ -4,12 +4,13 @@ define haproxy::balancer::server(
                                   $server_name   = $name,
                                   $check         = true,
                                   $backup        = false,
+                                  $order         = '00',
                                 ) {
   include ::haproxy
 
   concat::fragment{ "balancer-server: ${balancer_name} ${server_name}":
     target  => '/etc/haproxy/haproxy.cfg',
-    order   => "99-${balancer_name}-9",
+    order   => "99-${balancer_name}-9-${order}",
     content => template("${module_name}/balancer/server.erb"),
   }
 }
